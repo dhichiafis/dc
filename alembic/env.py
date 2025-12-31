7f9b1f42b05cd622d5103cedd37ef0db2d2e5d9f
dhichiafis <ochiengodhiambo56@gmail.com>
2025-12-31 14:53:37 -0800

    commit databes

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from models import User
+from models.model import User
 from database import Base 
 from alembic import context
 
--- schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class UserBase(BaseModel):
-    id:int 
-    username:str 
-
-class UserCreate(BaseModel):
-    username:str 
-    password:str 
