cca82389d06ee229a9ef05eae7e4a50f9272d2ab
dhichiafis <ochiengodhiambo56@gmail.com>
2026-01-25 11:02:37 -0800

    first payment disbursal

--- routes/bonuses.py
+++ routes/bonuses.py
@@ -105,7 +105,7 @@ def approve_bonus(bonus_id: int,
     if approve:
         try:
             #get_auth_token()
-            disburse_payments(amount=bonus.amount, phone_number=bonus.customer.phone_number)
+            disburse_payments(10, phone_number=bonus.customer.phone_number)
             bonus.paid_at = datetime.now()
         except Exception as e:
             db.rollback()
